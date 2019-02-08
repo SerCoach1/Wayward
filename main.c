52492e6953416711f0211431e816b529847f8ef3
Michael <michael.aug@protonmail.com>
2019-02-08 13:10:27 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#define _POSIX_C_SOURCE 200809L
+#include <assert.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <wayland-server.h>
+#include <wlr/backend.h>
+/*#include <wlr/render.h> Should be this, but no such file exists. Maybe updated to renderer.h?*/
+#include <wlr/render/wlr_renderer.h>
+
+int main(int argc, char **argv) {
+  printf("hello there\n");
+}
